dd65a5eadc76a1a34ee781af643e3b528284ef79
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-09-11 22:08:02 +0200

    Added simple RESTful web service with a H2 database to store and retrieve messages

--- src/main/kotlin/demo/KotlinSpringApplication.kt
+++ src/main/kotlin/demo/KotlinSpringApplication.kt
@@ -2,6 +2,15 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinSpringApplication
---
+++ src/main/kotlin/demo/service/MessageService.kt
@@ -0,0 +1,15 @@
+package demo.service
+
+import demo.model.Message
+import demo.respository.MessageRepository
+import org.springframework.stereotype.Service
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message) {
+        db.save(message)
+    }
+}
